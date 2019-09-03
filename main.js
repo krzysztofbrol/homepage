366de4a57a463277a2a4a2f919d59bd5b7343d07
Krzysztof Brol <krzysztof.brol@edu.uekat.pl>
2019-09-03 20:16:25 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema')
+const name = 'Krzysiek';
+const age = 26;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('witaj przybyszu 😂');
